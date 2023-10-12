f294fbe2489f3be8c545080c8d98c9df061369a5
Madhav Prabhu <dpmadhav@hotmail.com>
2023-10-12 23:39:08 +0530

    test

--- draw.js
+++ draw.js
@@ -10,6 +10,24 @@
 * // END : YOURNAME
 */
 
+//Test Madhav
+function square(side) {
+   repeat(4, function () {
+      forward(side);
+      right(90);
+   });
+}
+
+function demo() {
+   hideTurtle();
+   colour(0,0,255,1);
+   for(s = 100; s > 0; s -= 10) {
+      square(s);
+      right(36);
+   }
+}
+demo(5)
+//
 
 
 
