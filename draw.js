b963d125a009c28ade400a5b37dcd4e00d308f20
Madhav Prabhu <dpmadhav@hotmail.com>
2023-10-12 23:05:30 +0530

    Empty Canvas

--- draw.js
+++ draw.js
@@ -1,36 +1,17 @@
 // Write your code here
+// Do not Import anything
+/*
+* Your contribution should ideally be like this
+* Try to leave no gaps so that no one can mess with your code :)
+*
+* // BEGIN : YOURNAME
+* 	code()
+* 	...
+* // END : YOURNAME
+*/
 
-function spiral(steps, angle) {
-   const widthInc = 5 / steps;
-   let w = 0.1;
-   while (steps-- > 0) {
-      width(w);
-      forward(random(1,10));
-      right(angle--);
-      w += widthInc;
-   }
-}
 
-function fun(count) {
-   while (count-- > 0) {
-      colour(
-         random(0,255),
-         random(0,255),
-         random(0,255),
-         Math.random()
-      );
-      goto(random(-150, 150), random(-150, 150));
-      angle(random(0,360));
-      spiral(random(100, 1000), random(5, 90));
-   }
-}
 
-function demo(count) {
-   hideTurtle();
-   redrawOnMove(false);
-   fun(count);
-   redrawOnMove(true);
-   draw();
-}
 
-demo(100)
+// 
+
