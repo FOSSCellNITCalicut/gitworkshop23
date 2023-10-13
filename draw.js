7155b13b14d082e088e62b1e363db6394f45acdd
Madhav Prabhu <dpmadhav@hotmail.com>
2023-10-13 08:16:01 +0530

    letsgo

--- draw.js
+++ draw.js
@@ -10,6 +10,23 @@
 * // END : YOURNAME
 */
 
+function square(side) {
+   repeat(4, function () {
+      forward(side);
+      right(90);
+   });
+}
+
+function demo() {
+   hideTurtle();
+   colour(0,0,255,1);
+   for(s = 100; s > 0; s -= 10) {
+      square(s);
+      right(36);
+   }
+}
+
+demo(10)
 
 
 
