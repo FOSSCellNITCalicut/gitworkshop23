ecc4139746e552f8644bfa5a853fb2f3045cd45e
Joshua0305 <joshuajacobabraham@gmail.com>
2023-10-17 23:48:10 +0530

    back

--- draw.js
+++ draw.js
@@ -28,18 +28,3 @@ goto(100,100)
 square(100)
 // End : madhav
 
-//Begin: Joshua
-penup()
-left(90)
-forward(300)
-pendown()
-right(180)
-forward(60)
-left(90)
-forward(100)
-left(90)
-forward(80)
-right(180)
-forward(160)
-hideTurtle()
-//End: Joshua
